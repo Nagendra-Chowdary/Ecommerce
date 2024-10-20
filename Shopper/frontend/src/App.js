4fbb5eef684649dbdd3201e386dd2e68ff1c0926
Nagendra-Chowdary <chowdarynagendra006@gmail.com>
2024-10-20 20:19:49 +0530

    Added new components for home page

--- Shopper/frontend/src/App.js
+++ Shopper/frontend/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Shop from './pages/Shop'
+import {BrowserRouter,Routes,Route,Switch} from 'react-router-dom';
+import ShopCategory from './pages/ShopCategory';
+import Product from './pages/Product';
+import LoginSignup from './pages/LoginSignup';
+import Cart from './pages/Cart';
 function App (){
     return (
       <div>
+        <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop/>}></Route>
+          <Route path='/mens' element={<ShopCategory category="mens" />}></Route>
+          <Route path='/womens' element={<ShopCategory category="womens" />}></Route>
+          <Route path='/kids' element={<ShopCategory category="kids"/>}></Route>
+          <Route path='/cart' element={<Cart/>}></Route>
+          <Route path='/product' element={<Product/>}>
+          <Route path='/product/:productId' element={<Product/>}></Route>
+          </Route>
+          <Route path='/LoginSigup' element={<LoginSignup/>}></Route>
+        </Routes>
+        </BrowserRouter>
        
       </div>
     );
--- Shopper/frontend/src/components/Navbar/Navbar.jsx
+++ Shopper/frontend/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/Frontend_Assets/logo.png'
 import cart_icon from '../Assets/Frontend_Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 const Navbar=()=>{
     const [menu,setMenu]=useState("shop");
 return(
@@ -11,10 +12,10 @@ return(
         <p>SHOPPER</p>
     </div>
     <ul className="nav-menu">
-        <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr />:<React.Fragment></React.Fragment>}</li>
-        <li onClick={()=>{setMenu("men")}}>Men{menu==="men"?<hr />:<React.Fragment></React.Fragment>}</li>
-        <li onClick={()=>{setMenu("women")}}>Women{menu==="women"?<hr />:<React.Fragment></React.Fragment>}</li>
-        <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr />:<React.Fragment></React.Fragment>}</li>
+        <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration:'none',color:'black'}} to='/' >Shop</Link>{menu==="shop"?<hr />:<React.Fragment></React.Fragment>}</li>
+        <li onClick={()=>{setMenu("men")}}><Link to='/mens' style={{textDecoration:'none',color:'black'}} >Men</Link>{menu==="men"?<hr />:<React.Fragment></React.Fragment>}</li>
+        <li onClick={()=>{setMenu("women")}}><Link to='/womens' style={{textDecoration:'none',color:'black'}}>Women</Link>{menu==="women"?<hr />:<React.Fragment></React.Fragment>}</li>
+        <li onClick={()=>{setMenu("kids")}}><Link to='/kids' style={{textDecoration:'none',color:'black'}}>Kids</Link>{menu==="kids"?<hr />:<React.Fragment></React.Fragment>}</li>
     </ul>
     <div className="nav-login-cart">
         <button>Login</button>
